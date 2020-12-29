2a53c4e3d2782efa85f38c9d95564f878a64062a
harshit saxena <harshitsaxena586@gmail.com>
2020-12-29 21:32:13 +0530

    .

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var input = document.querySelector(#inputArea)
-
+var input = document.querySelector("#input")
+var show = document.querySelector("#output")
 var url="https://source.unsplash.com/1600x900/?"
 
-function linkGenerator(text){
-    return url+text
+// function linkGenerator(text){
+//     return <img src="https://source.unsplash.com/1600x900/?"+text>
+// }
+
+function getImage(){
+    return 
 }
 
+
+// show.innerHTML= getImage()
