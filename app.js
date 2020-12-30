fcfe0be1d14875e620a25f4540358bcd41124e71
harshit saxena <harshitsaxena586@gmail.com>
2020-12-30 20:28:07 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var input = document.querySelector("#input")
 var show = document.querySelector("#output")
+var button = document.querySelector("#click")
 var url="https://source.unsplash.com/1600x900/?"
 
 // function linkGenerator(text){
@@ -7,8 +8,18 @@ var url="https://source.unsplash.com/1600x900/?"
 // }
 
 function getImage(){
-    return 
+    return
 }
 
+function clickHandler(){
+show.innerHTML=  <img style="flex-grow: 1;" class="item" src="https://source.unsplash.com/random/
+" alt=""></img>
+}
+import { createClient } from 'pexels';
 
-// show.innerHTML= getImage()
+const client = createClient('563492ad6f9170000100000191f18bfbf0814fed8b2e33a4793ba8d0');
+const query = 'Nature';
+
+client.photos.search({ query, per_page: 1 }).then(photos => {...});
+
+button.addEventListener("click" ,clickHandler)
