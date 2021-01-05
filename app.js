d47b42e376eb1827c962bc52d70bd8b182301bc6
harshit saxena <harshitsaxena586@gmail.com>
2021-01-06 04:34:10 +0530

    added openweatherapi

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var input = document.querySelector("#input")
+var input = document.querySelector("#inputbox")
 var show = document.querySelector("#output")
 var button = document.querySelector("#click")
 
 
-
-function getImage(link){
-    holder = document.createElement("img")
-    holder.src = link
-    holder.className="item"
-    show.appendChild(holder)
+function urlGenerator(city){
+    return "api.openweathermap.org/data/2.5/weather?q="+city+"&appid=a01331da115ad1dcd7806395dde27639"
 }
 
-url ="https://api.pexels.com/v1"
-import { createClient } from 'pexels';
+function clickHandler(){
+    city = "London",
+    url = urlGenerator(city)
+    fetch("api.openweathermap.org/data/2.5/weather?q=London&appid=a01331da115ad1dcd7806395dde27639")
+     .then(response =>response.json())
+     .then(json=> console.log(json))
 
-const client = createClient('563492ad6f9170000100000191f18bfbf0814fed8b2e33a4793ba8d0');
 
-fetch(url)
-getImage("")
+}
+
+button.addEventListener("click", clickHandler())
