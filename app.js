818b1eb5056534e99060947e919fa35ea7dfd4de
harshit saxena <harshitsaxena586@gmail.com>
2021-01-06 04:02:08 +0530

    weather forcast

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 var input = document.querySelector("#input")
 var show = document.querySelector("#output")
 var button = document.querySelector("#click")
-var url="https://source.unsplash.com/1600x900/?"
 
-// function linkGenerator(text){
-//     return <img src="https://source.unsplash.com/1600x900/?"+text>
-// }
 
-function getImage(){
-    return
+
+function getImage(link){
+    holder = document.createElement("img")
+    holder.src = link
+    holder.className="item"
+    show.appendChild(holder)
 }
 
-function clickHandler(){
-show.innerHTML=  <img style="flex-grow: 1;" class="item" src="https://source.unsplash.com/random/
-" alt=""></img>
-}
+url ="https://api.pexels.com/v1"
 import { createClient } from 'pexels';
 
 const client = createClient('563492ad6f9170000100000191f18bfbf0814fed8b2e33a4793ba8d0');
-const query = 'Nature';
 
-client.photos.search({ query, per_page: 1 }).then(photos => {...});
-
-button.addEventListener("click" ,clickHandler)
+fetch(url)
+getImage("")
